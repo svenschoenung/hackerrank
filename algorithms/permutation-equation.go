d756467e708a843c904b993c44b3eb82f07fd7cd
Sven Schoenung <sven.schoenung@gmail.com>
2018-01-24 19:10:32 +0100

    Add permutation-equation solution

---
+++ algorithms/permutation-equation.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanf("%d\n", &n)
+
+	inv_p := make([]int, n + 1)
+	for x := 1; x <= n; x++ {
+		var px int
+		fmt.Scanf("%d", &px)
+		inv_p[px] = x
+	}
+
+	for x := 1; x <= n; x++ {
+		fmt.Printf("%d\n", inv_p[inv_p[x]])
+	}
+}
