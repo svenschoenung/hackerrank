93bfa155e3cf9a0277e6d11845a23ab01764894a
Sven Schoenung <sven.schoenung@gmail.com>
2018-01-28 12:49:44 +0100

    Add equality-in-a-array solution

---
+++ algorithms/equality-in-a-array.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanf("%d\n", &n)
+
+	max := 0
+	counts := make(map[int]int)
+	for i := 0; i < n; i++ {
+		var val int
+		fmt.Scanf("%d", &val)
+		counts[val]++
+		if counts[val] > max {
+			max = counts[val]
+		}
+	}
+
+	fmt.Printf("%d\n", n - max)
+}
